efaad32723e2f3f0066cb55c3d914bc067863eb4
wlandau <will.landau@gmail.com>
2015-03-28 13:52:12 -0500

    first commit

---
+++ R/getData.R
@@ -0,0 +1,20 @@
+mergeUSA2012 = function(){
+  f = "../cache/usa2012.rds"
+  if(file.exists(f)){
+    return(readRDS(f))
+  } else {
+    regions = c("United States of America", "Connecticut (USA)", "Florida (USA)", "Massachusetts (USA)")
+
+    load(url("http://beta.icm.edu.pl/PISAcontest/data/student2012.rda"))
+    usaStudent2012 = subset(student2012, CNT %in% regions)
+    rm(student2012)
+
+    load(url("http://beta.icm.edu.pl/PISAcontest/data/school2012.rda"))
+    usaSchool2012 = subset(school2012, CNT %in% regions)
+    rm(school2012)
+
+    usa2012 = merge(usaStudent2012, usaSchool2012, by="SCHOOLID")
+    saveRDS(usa2012, "../cache/usa2012.rds")
+    return(usa2012)
+  }
+}
