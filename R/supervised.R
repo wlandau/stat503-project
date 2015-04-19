58d70df7ac73a93afab9eea1ad0fb0bbc09960c4
wlandau <will.landau@gmail.com>
2015-04-19 14:24:01 -0500

    classifiers

--- R/supervised.R
+++ R/supervised.R
@@ -1,7 +1,121 @@
-
-
-randomforest = function(){
+error.rates = function(country = "USA", .issue = "top_20"){
+  library(class)
+  library(MASS)
   library(randomForest)
-  d = imputedUSA()
-  rf = randomForest(Performance ~., data = d)
+  library(nnet)
+  library(e1071)
+
+  source("../R/student.R")
+  source("../R/subsets.R")
+
+  lst = getSubset(country = country, .issue = .issue)
+
+  n = length(lst$y)
+  i = sample.int(n, floor(0.75*n))
+  t = 1:n %in% i
+
+  d = cbind(y = lst$y, lst$x)
+  train = d[t,]
+  test = d[!t,]
+
+  d.num = cbind(y = lst$y.num, lst$x)
+  train.num = d.num[t,]
+  test.num = d.num[!t,]
+
+  err = c()
+
+# logit
+
+  train.num01 = train.num
+  test.num01 = test.num
+
+  train.num01$y[train.num01$y < 1] = 0
+  test.num01$y[test.num01$y < 1] = 0
+  mylogit = glm(y~., data = train.num01, family = binomial)
+  yhat = predict(mylogit, newdata = test.num01, type = "response")
+  yhat = yhat > 0.5
+  err["logit"] = mean(yhat != test.num01$y)
+
+# randomForest 
+
+  rf = randomForest(y~., data = train)
+  yhat = predict(rf, newdata = test)
+  err["randomForest"] = mean(yhat != test$y)
+
+# randomForest(ntree = 10e4)
+
+  rf = randomForest(y~., data = train, ntree = 10e4)
+  yhat = predict(rf, newdata = test)
+  err["randomForest(ntree = 10e4)"] = mean(yhat != test$y)
+
+# bagging
+
+  m = ncol(train) - 1
+  rf = randomForest(y~., data = train, ntree = 10e4, mtry = m)
+  yhat = predict(rf, newdata = test)
+  err["bagging"] = mean(yhat != test$y)
+
+# neuralNet(size = 2)
+
+  listoffactors <- colnames(lst$x)
+  f = as.formula(paste("y~",paste(listoffactors,collapse="+")))
+  nn = nnet(f, data = train, size = 2)
+  yhat = predict(nn, newdata = test, type = "class")
+  err["neuralNet(size = 2)"] = mean(yhat != test$y)
+
+
+# neuralNet(size = m/4)
+
+  listoffactors <- colnames(lst$x)
+  f = as.formula(paste("y~",paste(listoffactors,collapse="+")))
+  nn = nnet(f, data = train, size = floor(m/4))
+  yhat = predict(nn, newdata = test, type = "class")
+  err["neuralNet(size = m/4)"] = mean(yhat != test$y)
+
+# neuralNet(size = 3m/4)
+
+  listoffactors <- colnames(lst$x)
+  f = as.formula(paste("y~",paste(listoffactors,collapse="+")))
+  nn = nnet(f, data = train, size = floor(3*m/4))
+  yhat = predict(nn, newdata = test, type = "class")
+  err["neuralNet(size = 3*m/4)"] = mean(yhat != test$y)
+
+# svm(linear)
+ 
+   sv = svm(y~., data = train, kernel = "linear") 
+   yhat = predict(sv, newdata = test)
+   err["svm(linear)"] = mean(yhat != test$y)
+
+# svm(cubic)
+ 
+   sv = svm(y~., data = train, kernel = "polynomial") 
+   yhat = predict(sv, newdata = test)
+   err["svm(cubic)"] = mean(yhat != test$y)
+
+# svm(radial)
+ 
+   sv = svm(y~., data = train, kernel = "radial") 
+   yhat = predict(sv, newdata = test)
+   err["svm(radial)"] = mean(yhat != test$y)
+
+# lda
+
+  ld = lda(y~., data = train)
+  yhat = predict(ld, newdata = test)$class
+   err["lda"] = mean(yhat != test$y)
+
+# qda
+
+  qd = qda(y~., data = train)
+  yhat = predict(qd, newdata = test)$class
+   err["qda"] = mean(yhat != test$y)
+  
+# nearestNeighbors
+
+  for(k in c(5, 10, 25)){
+    yhat = knn(train[,colnames(train) != "y"], test[colnames(test) != "y"], train$y, k = k)
+    err[paste("knn(k = ", k, ")", sep="")] = mean(yhat != test$y)
+  }
+
+  err
 }
